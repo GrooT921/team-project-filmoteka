53177a4795642da26f68fb002712397670236ffb
GrooT921 <99960856+GrooT921@users.noreply.github.com>
2022-09-07 11:55:41 +0000

    Deploying to gh-pages from @ GrooT921/team-project-filmoteka@1c69a7faf594fb40f24939f7cf05fba099e6f60e 🚀

--- index.daaccdfb.js
+++
@@ -1,2 +0,0 @@
-const e={searchSection:document.querySelector(".search"),libButtons:document.querySelector(".library-btn-list"),headerEl:document.querySelector("header"),libLink:document.querySelector("#library"),homeLink:document.querySelector("#home")};e.libLink.addEventListener("click",(function(t){t.preventDefault(),e.searchSection.classList.add("visually-hidden"),e.libButtons.classList.remove("visually-hidden"),e.homeLink.classList.remove("current"),e.libLink.classList.add("current"),e.headerEl.classList.add("header-container_lib")}));
-//# sourceMappingURL=index.daaccdfb.js.map
