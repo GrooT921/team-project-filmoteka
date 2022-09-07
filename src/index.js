c0fc031a88ad95cff0e79e06f689c0fb28723a21
KsanaSal <oasalivon@gmail.com>
2022-09-07 12:05:38 +0300

    Add get request

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import './sass/index.scss';
 import './js/library-header';
+import API_KEY from './js/apiKey';
+import getTrending from './js/getTrending';
+
+getTrending(API_KEY, 'movie', 'day').then(data => {
+    console.log(data);
+    //TODO: Тут потрібно запускати функцію відображення карток, додам як тільки з'являться шаблони карток
+});
