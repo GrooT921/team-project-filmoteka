18f160f3ce87a1f24fb8936cab1369e562835e72
KsanaSal <oasalivon@gmail.com>
2022-09-08 17:45:31 +0300

    fix rating and genres

--- src/js/createListMovies.js
+++ src/js/createListMovies.js
@@ -10,18 +10,22 @@ function listMovies(list) {
       .map(movie => {
         const genresName = movie.genre_ids
           .map(genreId => genres.filter(el => el.id === genreId)[0])
-          .map(el => el.name)
-          .join(', ');
-
+          .map(el => el.name);
+        const genresNameDotted =
+          genresName.length > 2
+            ? genresName.slice(0, 2).join(', ') + ' ...'
+            : genresName.join(', ');
         const year = new Date(movie.release_date).getFullYear();
         return `<li class="card__film">
         <div class="thumb">
-          <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+          <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${
+          movie.title
+        }" />
         </div>
         <h2 class="card__title">${movie.title}</h2>
         <p class="card__text">
-          <span class="genres">${genresName}</span> | <span class="release">${year}</span> <span
-            class="card__vote_average">${movie.vote_average}</span>
+          <span class="genres">${genresNameDotted}</span> | <span class="release">${year}</span> <span
+            class="card__vote_average">${movie.vote_average.toFixed(1)}</span>
         </p>
       </li>`;
       })
