9afca23808014c453b3d0f9018cae83fe6c9ab38
Elena-Sea <smetyukelena@gmail.com>
2022-09-08 19:25:04 +0200

    adds ls files

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './sass/index.scss';
 import API_KEY from './js/apiKey';
 import getTrending from './js/fetches/getTrending';
 import listMovies from './js/createListMovies';
+import toggleModal from './js/goit-modal';
 
 // Відображення популярних фільмів на головній сторінці
 getTrending(API_KEY, 'movie', 'week').then(data => {
